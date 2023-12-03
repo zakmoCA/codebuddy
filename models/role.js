cef5c3cfec233006e4fe8f3b87b6ad821d9c9f3a
zakmoCA <zakeriyaamohamed@gmail.com>
2023-12-03 15:08:34 +1000

    start with models

---
+++ models/role.js
@@ -0,0 +1,9 @@
+export default (sequelize, DataTypes) => {
+  const Role = sequelize.define('Role', {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  })
+  return Role
+}
