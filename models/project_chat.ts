59de45b66445471aa5431c1f219e7bd4cfeb13f5
zakmoCA <zakeriyaamohamed@gmail.com>
2023-12-05 02:17:14 +1000

    project chat model

--- models/project_chat.ts
+++ models/project_chat.ts
@@ -1,1 +1,42 @@
-//
+import { Sequelize, DataTypes, Model } from 'sequelize'
+import { User } from './user'
+import { Project } from './project'
+
+export interface ProjectChatAttributes {
+  id?: number
+  content: string
+  userId: number
+  projectId: number
+}
+
+export class ProjectChat extends Model<ProjectChatAttributes> implements ProjectChatAttributes {
+  public id!: number
+  public content!: string
+  public userId!: number
+  public projectId!: number
+  public readonly createdAt!: Date
+}
+
+export default (sequelize: Sequelize) => {
+  ProjectChat.init({
+    content: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: { model: User, key: 'id' }
+    },
+    projectId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: { model: Project, key: 'id' }
+    }
+  }, {
+    sequelize,
+    modelName: 'ProjectChat'
+  })
+
+  return ProjectChat
+}
