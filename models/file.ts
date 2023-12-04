815bef3866b4dc5486cb5c395a5c9888a10e452e
zakmoCA <zakeriyaamohamed@gmail.com>
2023-12-05 02:11:58 +1000

    file model

--- models/file.ts
+++ models/file.ts
@@ -1,1 +1,41 @@
-//
+import { Sequelize, DataTypes, Model } from 'sequelize'
+import { Project } from './project'
+
+export interface FileAttributes {
+  id?: number
+  name: string
+  content: string
+  projectId: number
+}
+
+export class File extends Model<FileAttributes> implements FileAttributes {
+  public id!: number
+  public name!: string
+  public content!: string
+  public projectId!: number
+  public readonly createdAt!: Date
+  public readonly updatedAt!: Date
+}
+
+export default (sequelize: Sequelize) => {
+  File.init({
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    content: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+    projectId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: { model: Project, key: 'id' }
+    }
+  }, {
+    sequelize,
+    modelName: 'File'
+  })
+
+  return File
+}
