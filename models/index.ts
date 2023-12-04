47fbef6bc304984b5284b2a980268d0ff4a01ac2
zakmoCA <zakeriyaamohamed@gmail.com>
2023-12-05 02:25:07 +1000

    ORM initialiser almost done, assosc. to go

--- models/index.ts
+++ models/index.ts
@@ -1,6 +1,11 @@
 import { Sequelize } from 'sequelize'
 import user, { User } from './user'
 import role, { Role } from './role'
+import project, { Project } from './project'
+import file, { File } from './file'
+import chatThread, { ChatThread } from './chat_thread'
+import message, { Message } from './message'
+import projectChat, { ProjectChat } from './project_chat'
 import { dbConfig } from '../config'
 
 interface Db {
@@ -8,6 +13,11 @@ interface Db {
   Sequelize: typeof Sequelize
   users: typeof User
   roles: typeof Role
+  projects: typeof Project
+  files: typeof File
+  chatThreads: typeof ChatThread
+  messages: typeof Message
+  projectChats: typeof ProjectChat
 }
 
 const sequelize = new Sequelize(dbConfig.database, dbConfig.username, dbConfig.password, {
@@ -19,10 +29,18 @@ const db: Db = {
   sequelize: sequelize,
   Sequelize: Sequelize,
   users: user(sequelize),
-  roles: role(sequelize)
+  roles: role(sequelize),
+  projects: project(sequelize),
+  files: file(sequelize),
+  chatThreads: chatThread(sequelize),
+  messages: message(sequelize),
+  projectChats: projectChat(sequelize)
 }
+
 
 db.users.hasMany(db.roles)
 db.roles.belongsTo(db.users)
+
+// flesh out relationships in next commit-batch
 
 export default db
