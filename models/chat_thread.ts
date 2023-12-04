1058376118da438879ffc7624289608b7ff5beb5
zakmoCA <zakeriyaamohamed@gmail.com>
2023-12-05 02:13:01 +1000

    chat-thread model

--- models/chat_thread.ts
+++ models/chat_thread.ts
@@ -1,1 +1,27 @@
-//
+import { Sequelize, DataTypes, Model } from 'sequelize'
+
+export interface ChatThreadAttributes {
+  id?: number
+  name: string
+}
+
+export class ChatThread extends Model<ChatThreadAttributes> implements ChatThreadAttributes {
+  public id!: number
+  public name!: string
+  public readonly createdAt!: Date
+  public readonly updatedAt!: Date
+}
+
+export default (sequelize: Sequelize) => {
+  ChatThread.init({
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  }, {
+    sequelize,
+    modelName: 'ChatThread'
+  })
+
+  return ChatThread
+}
