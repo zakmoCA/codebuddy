15dc01c969cd9258ea5defd891cad831d9340b8a
zakmoCA <zakeriyaamohamed@gmail.com>
2023-12-06 08:44:58 +1000

    user login/registration/profile-update actions scaffold

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,11 +1,53 @@
-function registerUser(userData) {
-  
+function registerUser(req, res) {
+  let body = ''
+
+  req.on('data', chunk => {
+      body += chunk.toString()
+  })
+
+  req.on('end', () => {
+      const userData = JSON.parse(body)
+
+      // registration logic
+
+      res.writeHead(201, { 'Content-Type': 'application/json' })
+      res.end(JSON.stringify({ message: 'User registered successfully' }))
+  })
 }
 
-function loginUser(credentials) {
-  
+
+function loginUser(req, res) {
+  let body = ''
+
+  req.on('data', chunk => {
+      body += chunk.toString()
+  })
+
+  req.on('end', () => {
+      const credentials = JSON.parse(body)
+
+      // do login logic soon
+
+      res.writeHead(200, { 'Content-Type': 'application/json' })
+      res.end(JSON.stringify({ message: 'User logged in successfully' }))
+  })
 }
 
-function updateUserProfile(userId, updatedData) {
-  
+
+function updateUserProfile(req, res, userId) {
+  let body = ''
+
+  req.on('data', chunk => {
+      body += chunk.toString()
+  })
+
+  req.on('end', () => {
+      const updatedData = JSON.parse(body)
+
+      // do updat profile logic soon
+
+      res.writeHead(200, { 'Content-Type': 'application/json' })
+      res.end(JSON.stringify({ message: 'Your profile updated successfully!' }))
+  })
 }
+
