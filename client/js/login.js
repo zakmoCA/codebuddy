be8adf79a32370574071f12edad49c3ab2958fa9
zakmoCA <zakeriyaamohamed@gmail.com>
2023-12-09 23:07:39 +1000

    login markup and var init

--- client/js/login.js
+++ client/js/login.js
@@ -1,13 +1,27 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const usernameInput = document.getElementById('username')
+  const passwordInput = document.getElementById('password')
+  const loginButton = document.getElementById('login-button')
+  const forgotPasswordLink = document.getElementById('forgot-password')
+  const signupButton = document.getElementById('signup-button')
+
+  // listeners
+  
+})
+
+
+
 function registerUser() {
-  //
+  
+
 }
 
 
 function loginUser() {
-  //
+  
 }
 
 
-function updateProfile() {
-  //
+function updatePassword() {
+  
 }
